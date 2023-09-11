396e499c05a518f8633a7808a9fb246b7866e999
Zecheng Wang <zechengw@usc.edu>
2023-09-10 20:01:48 -0700

    ReadFile Library is developing

--- test/main.cc
+++ test/main.cc
@@ -1,10 +1,20 @@
 #include <iostream>
+#include <string.h>
 #include "NetworkAnalyzer/NetworkAnalyzer.h"
+#include "ReadFile/ReadFile.h"
+
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
     WhatsMyHostname();
+    
+    string path = argv[1];
+    int readSz = 128;
+    char buf[readSz];
+    memset(buf, 0, sizeof(buf));
+    ReadFileWithRange(path, 0, readSz-1, buf);
+    cout << buf << endl;
     return 0;
 }
